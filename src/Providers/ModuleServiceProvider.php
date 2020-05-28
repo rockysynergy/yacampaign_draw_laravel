b9462c5302a8331a81de1a3b17aa39c2b6f04157
Rocky Ou <rockysynergy@qq.com>
2020-05-28 09:44:14 +0800

    Created the module

---
+++ src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Orqlog\YacampaignDraw\Providers;
+
+use Konekt\Concord\BaseModuleServiceProvider;
+
+class ModuleServiceProvider extends BaseModuleServiceProvider
+{
+}
---
+++ src/resources/manifest.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'    => 'Laravel Campain Draw',
+    'version' => '0.0.1'
+ ];
